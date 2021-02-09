d579c3808e1be89c464ea2e6225dbda8d9246b76
pdgper <pdgper@gmail.com>
2021-02-09 15:48:37 +0000

    Day 1 Completed

---
+++ day1/js/scripts.js
@@ -0,0 +1,11 @@
+let numberTen = 10;
+let letterA = "A";
+
+console.log(typeof numberTen);
+console.log(typeof letterA);
+
+let firstVar = 1,
+    secondVar = 2
+    thirdVar = 3;
+
+console.log(firstVar, secondVar, thirdVar);
