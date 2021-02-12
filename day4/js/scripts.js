3b05e782cf108686d8dd2bd17118f71b8383b036
pdgper <pdgper@gmail.com>
2021-02-12 20:43:14 +0000

    Day 4 Part 1

---
+++ day4/js/scripts.js
@@ -0,0 +1,38 @@
+/*
+let userAge = prompt("Enter your age:");
+
+if (userAge > 18) {
+    console.log("You are old enough to drive.");
+} else {
+    console.log(`You still have to wait ${18 - userAge} years to drive.`);
+};
+*/
+
+/*
+let userAge = prompt("Enter your age:");
+let myAge = 35;
+
+if (userAge > myAge) {
+    console.log(`You are ${userAge - myAge} year(s) older than me.`);
+} else if (userAge == myAge) {
+    console.log(`You are the same age as me.`);
+} else {
+    console.log(`You are ${myAge - userAge} year(s) younger than me.`);
+};
+*/
+
+/*
+let a = 4;
+let b = 3;
+
+if (a > b) {
+    console.log(`${a} is greater than ${b}.`);
+} else {
+    console.log(`${b} is greater than ${a}.`);
+};
+
+a > b 
+    ? console.log(`${a} is greater than ${b}.`)
+    : console.log(`${b} is greater than ${a}.`);
+*/
+
