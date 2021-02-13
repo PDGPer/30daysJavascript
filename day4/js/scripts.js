857dc5915559bab683d286366961f80a1658077c
pdgper <pdgper@gmail.com>
2021-02-13 23:01:31 +0000

    Day 4 Part 2

--- day4/js/scripts.js
+++ day4/js/scripts.js
@@ -36,3 +36,83 @@ a > b
     : console.log(`${b} is greater than ${a}.`);
 */
 
+/*
+let inputNumber = prompt("Enter a number:");
+
+if (inputNumber % 2 === 0) {
+    console.log(`${inputNumber} is even.`);
+} else {
+    console.log(`${inputNumber} is odd.`);
+}
+*/
+
+/*
+Write a code which can give grades to students according to theirs scores:
+
+    90-100, A
+    70-89, B
+    60-69, C
+    50-59, D
+    0-49, F
+*/
+
+/*
+let grade = prompt("Enter a grade:");
+
+if (grade > 89 && grade < 101) {
+    console.log("Grade A");
+} else if (grade > 69 && grade < 100) {
+    console.log("Grade B");
+} else if (grade > 59 && grade < 100) {
+    console.log("Grade C");
+} else if (grade > 49 && grade < 100) {
+    console.log("Grade D");
+} else if (grade >= 0 && grade < 50) {
+    console.log("Grade F");
+} else {
+    console.log("Not a valid grade.");
+};
+*/
+
+/*
+Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+
+    September, October or November, the season is Autumn.
+    December, January or February, the season is Winter.
+    March, April or May, the season is Spring
+    June, July or August, the season is Summer
+
+*/
+
+/*
+let season = prompt("Enter a season:");
+
+if (season === "September" || season === "October" || season === "November") {
+    console.log("The season is Autumn.");
+} else if (season === "December" || season === "January" || season === "February") {
+    console.log("The season is Winter.");
+} else if (season === "March" || season === "April" || season === "May") {
+    console.log("The season is Spring.");
+} else if (season === "June" || season === "July" || season === "August") {
+    console.log("The season is Summer.");
+} else {
+    console.log("Not a valid season.");
+};
+*/
+
+/*
+Check if a day is weekend day or a working day. Your script will take day as an input.
+
+What is the day  today? Saturday
+Saturday is a weekend.
+
+What is the day today? saturDaY
+Saturday is a weekend.
+
+What is the day today? Friday
+Friday is a working day.
+
+What is the day today? FrIDAy
+Friday is a working day.
+*/
+
